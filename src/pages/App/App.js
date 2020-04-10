f80425317737cb0300a3c537140e7dad679f6ee4
ADAM <zebolsky.adam@gmail.com>
2020-04-10 16:23:31 -0500

    update client-side routes

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <nav>
+          <nav className="App-nav-container">
             <NavLink className="App-nav-title" exact to="/">
               BeerNear
             </NavLink>
@@ -16,6 +16,9 @@ class App extends Component {
             </NavLink>
             <NavLink className="App-nav" exact to="/login">
               Login
+            </NavLink>
+            <NavLink className="App-nav" exact to="/fridge">
+              My Fridge
             </NavLink>
           </nav>
         </header>
